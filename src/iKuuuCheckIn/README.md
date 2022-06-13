0a0f00c4215e2777200096f694e053da7c3d2b1a
ZiuChen <457353192@qq.com>
2022-06-13 10:35:57 +0800

    feat: 新增脚本`iKuuuCheckIn`

---
+++ src/iKuuuCheckIn/README.md
@@ -0,0 +1,5 @@
+## iKuuuCheckIn
+
+所有内容与`v2FreeAutoCheckIn`相同，请跳转查看：
+
+[v2FreeAutoCheckIn - README.md](../v2FreeAutoCheckIn/README.md)
---
+++ src/iKuuuCheckIn/index.js
@@ -0,0 +1,40 @@
+/**
+ * @name iKuuuCheckIn.js
+ * @author ZiuChen
+ * @version 0.1
+ * @Licence MIT
+ */
+
+const fetch = require("node-fetch")
+const PushPlus = require("./lib/PushPlus")
+const iKuuuCookie = process.env["iKuuuCookie"]
+const iKuuuURL = "https://ikuuu.ltd/user/checkin"
+
+const CheckIn = async () => {
+  if (!iKuuuCookie) {
+    console.log("环境变量iKuuuCookie未定义")
+    return false
+  }
+  return fetch(iKuuuURL, {
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      cookie: iKuuuCookie
+    }
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      const result = `iKuuu已签到: ${json.msg}`
+      console.log(result)
+      return PushPlus({
+        title: "iKuuu已签到",
+        content: result
+      })
+    })
+    .catch((err) => console.log(err))
+}
+
+exports.main_handler = async () => {
+  return await CheckIn()
+}
