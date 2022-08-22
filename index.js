cef23d19a976e81d3fcd5c5f900da2d43c053896
ZiuChen <457353192@qq.com>
2022-08-22 22:26:22 +0800

    refactor: 移除DR相关入口

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const { main_handler: v2Free } = require("./src/v2FreeAutoCheckIn")
-const { main_handler: iKuuu } = require("./src/iKuuuCheckIn")
-const { main_handler: dailyReport } = require("./src/BJTUDailyReport")
+const { main_handler: v2Free } = require('./src/v2FreeAutoCheckIn')
+const { main_handler: iKuuu } = require('./src/iKuuuCheckIn')
 
 exports.main_handler = async () => {
   await v2Free()
   await iKuuu()
-  await dailyReport()
 }
