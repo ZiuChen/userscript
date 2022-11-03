a04cda646632afb9298d91fed65ee926bd07b1fd
ZiuChen <457353192@qq.com>
2022-11-03 09:35:22 +0800

    feat: 添加GlaDOS签到功能

---
+++ src/GlaDOSCheckIn/index.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch')
+const PushPlus = require('../lib/PushPlus')
+const GlaDOSCookie = process.env['GlaDOSCookie']
+
+const checkIn = async () => {
+  const url = 'https://glados.rocks/api/user/checkin'
+  if (!GlaDOSCookie) {
+    console.log('环境变量GLADOSCookie未定义')
+    return false
+  }
+  return fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      cookie: GlaDOSCookie,
+      origin: 'https://glados.rocks',
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+      'content-type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+      token: 'glados.network'
+    })
+  }).then((res) => res.json())
+}
+
+const status = async () => {
+  const url = 'https://glados.rocks/api/user/status'
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      cookie: GlaDOSCookie
+    }
+  })
+}
+
+const CheckIn = async () => {
+  let checkInMessage = ''
+  let leftDays = 0
+  await checkIn().then((json) => {
+    checkInMessage = json?.message
+  })
+  await status()
+    .then((res) => res.json())
+    .then((json) => {
+      leftDays = parseInt(json?.data?.leftDays)
+    })
+  console.log(leftDays, checkInMessage)
+  await PushPlus({
+    title: 'GLaDOS签到',
+    content: `签到结果: ${checkInMessage}, 剩余天数: ${leftDays}`
+  })
+}
+
+exports.main_handler = async () => {
+  return CheckIn()
+}
--- src/v2FreeAutoCheckIn/README.md
+++ src/v2FreeAutoCheckIn/README.md
@@ -4,7 +4,7 @@
 
 * **需配置环境变量 `PushPlusToken` 与 `v2freeCookie`**
   *  `PushPlusToken` 访问[此网站](http://www.pushplus.plus/)申请，每日有免费推送额度
-  * `v2freeCookie` 可以携带登录状态访问[此网站](https://w1.v2free.net/user/checkin)，从 Network 中抓取，经过测试，此Cookie长期有效
+  * `v2freeCookie` 可以携带登录状态访问对应机场网站，从 F12 Network 中抓取，经过测试，除iKuuu外，其他机场Cookie长期有效
 * **由于不同checker位于不同的目录下，需要分别安装依赖**
 * **Node-Fetch版本为 `2.6.7`，安装时需额外指定版本**
 
